b5dba7ef32e52c293d940932c58c41ba0e0f6797
Grigorij Schleifer <chaosambulance@googlemail.com>
2022-08-21 22:10:08 +0200

    Getting ther, not pretty but ...

--- Lesson_1.py
+++ Lesson_1.py
@@ -6,11 +6,13 @@ drug_list = [
 
 combinations = []
 
+# first loop to extract the first drug
 for drug in drug_list:
-    for index_drug in range(len(drug_list)):
-        two_drugs = [drug, drug_list[1:len(drug_list)]]
+    # second loop to 
+    for index in range(len(drug_list)):
+        two_drugs = [drug, drug_list[1]], [drug, drug_list[2]], [drug, drug_list[3]]
         combinations.append(two_drugs)
-        drug_list = drug_list[:len(drug_list)]
+    drug_list.remove(drug)
 
 for i in combinations:
     print(i)
