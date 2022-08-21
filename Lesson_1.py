c7e9613682651f36e4e057ab869bc63a2f18b6c2
Grigorij Schleifer <chaosambulance@googlemail.com>
2022-08-21 22:28:04 +0200

    Getting there ...

--- Lesson_1.py
+++ Lesson_1.py
@@ -4,15 +4,8 @@ drug_list = [
     "co-codamol",
     "vitamin b12"]
 
-combinations = []
-
 # first loop to extract the first drug
 for drug in drug_list:
     # second loop to 
-    for index in range(len(drug_list)):
-        two_drugs = [drug, drug_list[1]], [drug, drug_list[2]], [drug, drug_list[3]]
-        combinations.append(two_drugs)
-    drug_list.remove(drug)
-
-for i in combinations:
-    print(i)
+    for i in drug_list[1:]:
+        print("[" + str(drug) + "," + str(i) + "]") 
