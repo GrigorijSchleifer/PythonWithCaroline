67ec83b28c62749ab91911d61eed19837f859641
Grigorij Schleifer <chaosambulance@googlemail.com>
2022-08-21 21:58:18 +0200

    Struggling ... but getting there

--- Lesson_1.py
+++ Lesson_1.py
@@ -4,3 +4,13 @@ drug_list = [
     "co-codamol",
     "vitamin b12"]
 
+combinations = []
+
+for drug in drug_list:
+    for index_drug in range(len(drug_list)):
+        two_drugs = [drug, drug_list[1:len(drug_list)]]
+        combinations.append(two_drugs)
+        drug_list = drug_list[:len(drug_list)]
+
+for i in combinations:
+    print(i)
