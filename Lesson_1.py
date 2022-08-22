9aeaeff32097ef44b3b63289567917b021af4baa
Grigorij Schleifer <chaosambulance@googlemail.com>
2022-08-22 20:49:18 +0200

    Not really a solution

--- Lesson_1.py
+++ Lesson_1.py
@@ -6,13 +6,25 @@ drug_list = [
 
 combinations = []
 
-# first loop to extract the first drug
-for drug in drug_list:
-    # second loop to 
-    for i in drug_list:
-        if i == drug:
-            continue
-        combinations.append("[" + str(drug) + "," + str(i) + "]") 
 
-for item in combinations:
-    print(item)
+# Write a function that takes in a list of drugs, and returns all 2-drug combinations as a list of pairs. 
+def task_one(list_of_drugs):
+    # first loop to extract the first drug
+    for drug in drug_list:
+        # second loop to 
+        for i in drug_list:
+            combinations.append([drug, i])
+    return combinations
+
+# Write a function that takes in a list of drugs, returns all unique 2-drug combinations as a list of pairs
+def task_two(list_of_combinations):
+    for drug in list_of_combinations:
+        if drug[0] == drug[1]:
+            list_of_combinations.pop(drug)
+    return list_of_combinations 
+
+# create combination list 
+listcombinations = task_one(drug_list)
+print(unique_combinations)
+# print unique pairs 
+print(task_two(list_of_combinations))
