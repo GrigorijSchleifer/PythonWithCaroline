ae2a826026cdce91af702f9c82126ff1182bf295
Grigorij Schleifer <chaosambulance@googlemail.com>
2022-08-24 21:30:26 +0200

    Carries review implermentation

--- Lesson_1.py
+++ Lesson_1.py
@@ -11,16 +11,24 @@ drug_list = [
 combinations = []
 
 
-# Write a function that takes in a list of drugs, and returns all 2-drug combinations as a list of pairs. 
-def task_one(list_of_drugs):
-    # first loop to extract the first drug
-    for drug in drug_list:
+def make_combinations(list_of_items: list) -> list:
+    """
+    Take list of items and return all 2-item combinations 
+    
+    Args:
+    list_of_items (list): a list of items to be combined
+
+    Returns:
+    list: A list of lists of pairs
+    """
+    # first loop to extract the first item
+    for item in list_of_items:
         # second loop to 
-        for i in drug_list:
-            combinations.append([drug, i])
+        for second_item in list_of_items:
+            combinations.append([item, second_item])
     return combinations
 
-all_combinations = task_one(drug_list)
+all_combinations = make_combinations(drug_list)
 # print list with all combinations
 for i in all_combinations:
     print(i)
@@ -29,13 +37,22 @@ for i in all_combinations:
 print("\n")
     
 # Write a function that takes in a list of drugs, returns all unique 2-drug combinations as a list of pairs
-def task_two(list_of_combinations):
-    for idx, drug in enumerate(list_of_combinations):
-        if drug[0] == drug[1] or drug[::-1] in list_of_combinations:
-            list_of_combinations.pop(idx)
-    return list_of_combinations 
+def make_unique_combinations(list_of_items: list) -> list:
+    """
+    Take list of items and return all unique 2-item combinations 
+    
+    Args:
+    list_of_items (list): a list of items to be filtered for unique combinations
 
-unique_combinations = task_two(all_combinations)
+    Returns:
+    list: A list of lists of unique pairs
+    """
+    for idx, drug in enumerate(list_of_items):
+        if drug[0] == drug[1] or drug[::-1] in list_of_items:
+            list_of_items.pop(idx)
+    return list_of_items 
+
+unique_combinations = make_unique_combinations(all_combinations)
 # print unique pairs 
 for i in unique_combinations:
     print(i)
