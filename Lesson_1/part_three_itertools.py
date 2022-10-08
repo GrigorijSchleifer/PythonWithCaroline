9bd8f0ec1c6b9a20531b77250645c665e214ce40
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-10-08 10:29:00 +0200

    tweaked color coding and something else ... can't remember

--- Lesson_1/part_three_itertools.py
+++ Lesson_1/part_three_itertools.py
@@ -1,4 +1,13 @@
 from itertools import combinations
+ 
+"""
+Use combinations module from itertools package 
+Not clear at all what is going on under the hood
+
+Args: Not clear
+
+Return: List
+"""
 
 drug_list = [
     "paracetamol",
@@ -7,7 +16,9 @@ drug_list = [
     "vitamin b12",
     "metoprolol"
     ]
+   
 
 combis = list(combinations(drug_list, 3))
+
 for itm in combis:
     print(itm)
